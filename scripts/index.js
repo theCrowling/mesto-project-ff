5af4f081c519cac68bf678006c1bd7f05218e96f
Crowling <daniel22r@yandex.ru>
2024-10-28 21:28:04 +0300

    Final v2

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,13 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
-function addCard(content, deleteCallback) {
+function createCard(content, deleteCallback) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button');
+  const cardImage = cardElement.querySelector('.card__image');
   cardElement.querySelector('.card__title').textContent = content.name;
-  cardElement.querySelector('.card__image').src = content.link;
+  cardImage.src = content.link;
+  cardImage.alt = content.name;
   deleteButton.addEventListener('click', deleteCallback);
   return cardElement;
 };
@@ -21,6 +23,6 @@ function deleteCard(evt) {
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (card) {
-  cardsContainer.append(addCard(card, deleteCard));
+  cardsContainer.append(createCard(card, deleteCard));
 });
 
