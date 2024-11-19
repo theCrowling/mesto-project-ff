857d3644d6ace47d02e981914389a57d130b1e8a
Crowling <daniel22r@yandex.ru>
2024-11-19 21:34:47 +0300

    v2.3.1 Ready project (new card)

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,27 +1,27 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Винтерфелл",
+      link: "https://www.kino-teatr.ru/news/9566/94687.jpg",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Черный замок",
+      link: "https://www.syfy.com/sites/syfy/files/2019/04/game_of_thrones_castle_black_via_hbo_site_2019.jpg",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Стена",
+      link: "https://www.focus.it/site_stored/imgs/0005/012/game-of-thrones-the-wall-history-03.jpg",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Суровый дом",
+      link: "https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/26606632-8c81-42ba-9487-b20d1fa12298/1920x",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Драконий камень",
+      link: "https://i.pinimg.com/originals/49/24/08/492408222398b56f0bda84f9388f1473.jpg",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Королевская гавань",
+      link: "https://avatars.mds.yandex.net/i?id=8edfc87ebca79b060b9c91ce691502b0_l-5465325-images-thumbs&n=13",
     }
 ];
 
