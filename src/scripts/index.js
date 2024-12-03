2f497d20db9327162309c27fc3e9afc59fbb0412
Crowling <daniel22r@yandex.ru>
2024-12-03 22:18:52 +0300

    v3.1 Validation ready

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,11 +3,23 @@ import '../pages/index.css';
 import {initialCards} from './cards.js';
 import {createCard, deleteCard, likeCard} from './card.js';
 import {openModal, closeModal} from './modal.js';
+import {enableValidation, clearValidation} from './validation.js';
+// Конфигурация валидации форм
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active'
+};
 
 //Тут описана инициализация приложения и основная логика страницы: поиск DOM-элементов на странице и навешивание на них обработчиков событий; обработчики отправки форм, функция-обработчик события открытия модального окна для редактирования профиля; функция открытия модального окна изображения карточки.
 
-// Основные переменные
+// Поиск DOM элементов
+// Контейнер для карточек
 const cardsContainer = document.querySelector('.places__list');
+// Кнопки открытия модальных окон
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileAddButton = document.querySelector('.profile__add-button');
 // Модальные окна
@@ -16,34 +28,58 @@ const modalAdd = document.querySelector('.popup_type_new-card');
 const modalImage = document.querySelector('.popup_type_image');
 const modalCardImage = document.querySelector('.popup__image');
 const modalCardTitle = document.querySelector('.popup__caption');
-
-// Слушатели для модального окна
-profileEditButton.addEventListener('click', () => openModal(modalEdit));
-profileAddButton.addEventListener('click', () => openModal(modalAdd));
-
 // Форма редактирования профиля
 const editFormElement = document.forms['edit-profile'];
 const nameInput = document.querySelector('.profile__title');
 const jobInput = document.querySelector('.profile__description');
-editFormElement.elements.name.value = nameInput.textContent;
-editFormElement.elements.description.value = jobInput.textContent;
+const newName = editFormElement.elements.name;
+const newJob = editFormElement.elements.description;
 // Форма добавления новой карточки
 const addFormElement = document.forms['new-place'];
-// Обработчики отправки форм
+const newPlace = addFormElement.elements['place-name'];
+const newLink = addFormElement.elements['link'];
+
+// Слушатели открытия модального окна
+profileEditButton.addEventListener('click', handleEditButtonClick);
+profileAddButton.addEventListener('click', handleAddButtonClick);
+
+// Функция обработчик открытия модального окна для редактирования профиля
+function handleEditButtonClick() {
+  newName.value = nameInput.textContent;
+  newJob.value = jobInput.textContent;
+  clearValidation(editFormElement, validationConfig);
+  openModal(modalEdit)
+};
+
+// Функция обработчик открытия модального окна для добавления новой карточки
+function handleAddButtonClick() {
+  newPlace.value = '';
+  newLink.value = '';
+  clearValidation(addFormElement, validationConfig);
+  openModal(modalAdd)
+};
+
+// Функция открытия модального окна изображения карточки
+function openModalImage(card) {
+  modalCardImage.src = card.link;
+  modalCardImage.alt = card.name;
+  modalCardTitle.textContent = card.name;
+  openModal(modalImage);
+};
+
+// Слушатели отправки форм
 editFormElement.addEventListener('submit', handleEditFormSubmit);
 addFormElement.addEventListener('submit', handleAddFormSubmit);
 
-// Функция-обработчик события открытия модального окна для редактирования профиля
+// Функция-обработчик события submit для окна редактирования профиля
 function handleEditFormSubmit(evt) {
   evt.preventDefault();
-  const newName = editFormElement.elements.name.value;
-  const newJob = editFormElement.elements.description.value;
-  nameInput.textContent = newName;
-  jobInput.textContent = newJob;
+  nameInput.textContent = newName.value;
+  jobInput.textContent = newJob.value;
   closeModal(modalEdit);
 };
 
-// Функция-обработчик события открытия модального окна для добавления новой карточки
+// Функция-обработчик события submit для окна добавления новой карточки
 function handleAddFormSubmit(evt) {
   evt.preventDefault();
   const newPlace = addFormElement.elements['place-name'].value;
@@ -53,17 +89,10 @@ function handleAddFormSubmit(evt) {
   addFormElement.reset();
 };
 
-// Функция открытия модального окна изображения карточки
-function openModalImage(card) {
-  modalCardImage.src = card.link;
-  modalCardImage.alt = card.name;
-  modalCardTitle.textContent = card.name;
-  openModal(modalImage);
-}
-
-// Вывести карточки на страницу
+// Вывод карточек на страницу
 initialCards.forEach(function (card) {
-  cardsContainer.append(createCard(card, deleteCard, likeCard, () => openModalImage(card)));
+  cardsContainer.append(createCard(card, deleteCard, likeCard, openModalImage));
 });
 
-
+// Включение валидации, все настройки передаются объектом конфигурации
+enableValidation(validationConfig);
---
+++ src/scripts/validation.js
@@ -0,0 +1,92 @@
+// Тут описаны функции для валидации форм. Из файла экспортируется только функция активации валидации enableValidation и функция очистки ошибок валидации clearValidation;
+
+// Функция, которая включает в себя валидацию всех форм сайта
+function enableValidation(config) {
+  const formList = Array.from(document.querySelectorAll(config.formSelector));
+
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement, config);
+  });
+}
+
+// Функция для очистки ошибок валидации
+function clearValidation(formElement, config) {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+
+  inputList.forEach((inputElement) => {
+    hideInputError(formElement, inputElement, config);
+    inputElement.setCustomValidity('');
+  });
+
+  toggleButtonState(inputList, buttonElement, config);
+
+}
+
+// Функиця для навешивания слушателей на инпуты
+function setEventListeners(formElement, config) {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+
+  toggleButtonState(inputList, buttonElement, config);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement, config);
+      toggleButtonState(inputList, buttonElement, config);
+    });
+  });
+}
+
+// Функия переключения кнопки в зависимости от валидности инпутов
+function toggleButtonState(inputList, buttonElement, config) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+    buttonElement.classList.add(config.inactiveButtonClass);
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove(config.inactiveButtonClass);
+  }
+}
+
+// Функция проверки валидности инпутов
+function checkInputValidity(formElement, inputElement, config) {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+  } else {
+    inputElement.setCustomValidity('');
+  }
+
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, config);
+  } else {
+    hideInputError(formElement, inputElement, config);
+  }
+}
+
+// Функция для показа ошибок
+function showInputError(formElement, inputElement, errorMessage, config) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(config.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(config.errorClass);
+}
+
+// Функция для скрытия ошибок
+function hideInputError(formElement, inputElement, config) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(config.inputErrorClass);
+  errorElement.classList.remove(config.errorClass);
+  errorElement.textContent = '';
+}
+
+// Функция проверки наличия невалидных инпутов
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => !inputElement.validity.valid);
+}
+
+export { enableValidation, clearValidation };
